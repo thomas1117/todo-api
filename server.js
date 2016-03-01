7c626124d65f043b153b43fc8af4704e7c32b254
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2016-02-29 19:44:15 -0800

    added validation and trim on description

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -17,26 +18,25 @@ app.get('/todos', function(req,res){
 })
 app.get('/todos/:id', function(req,res){
 	var todoId = parseInt(req.params.id,10);
-	var match;
-	var one = todos.forEach(function(todo){
-		if(todo.id===todoId) {
-			match = todo;
-		}
-	})
-	if(match) {
-	res.json(match);
-	} else {
-		res.status(404).send();
-	}
+	var match = _.findWhere(todos,{id:todoId})
+	
+	match ? res.json(match) : res.send("404 error"); 
+		
+	
 });
 
 app.post('/todos', function(req,res){
-	var body = req.body;
-	console.log('description ' + body.description);
-	res.json(body);
+	var body = _.pick(req.body,'description','completed')
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0) {
+		return res.status(400).send();
+	}
+	
+	body.description = body.description.trim();
 	body.id = todoNextId++;
+
 	todos.push(body);
-})
+	res.json(body);
+});
 //GET /todos/:id
 app.listen(PORT, function(){
 	console.log('Listening on ' + PORT)
