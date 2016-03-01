e6cd979d0f152685d7888a3969b96f47e1a7b59c
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2016-02-29 20:40:41 -0800

    added put todos

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/todos/:id', function(req,res){
 });
 
 app.post('/todos', function(req,res){
-	var body = _.pick(req.body,'description','completed')
+	var body = _.pick(req.body,'description','completed');
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0) {
 		return res.status(400).send();
 	}
@@ -49,6 +49,32 @@ app.delete('/todos/:id', function(req,res){
 	} else {
 		res.send("404 error");
 	}
+});
+
+app.put('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id,10);
+	var match = _.findWhere(todos,{id:todoId});
+	var body = _.pick(req.body,'description','completed');
+	var validAttributes={};
+
+	if(!match) {
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed')&& _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description')&& _.isString(body.description)&& body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		res.status(400).send();
+	}
+	
+	_.extend(match,validAttributes);
+	res.json(match);
 });
 
 app.listen(PORT, function(){
