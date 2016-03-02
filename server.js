19238e192e8ab3dd902ba95d6f43290ceb17fec8
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2016-03-02 01:28:56 -0800

    added todo model and post todo

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -52,15 +54,21 @@ app.get('/todos/:id', function(req, res) {
 
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function(todo){
+		res.status(200)
+		res.json(todo.toJSON())
+	}).catch(function(e){
+		res.status(400).json(e);
+	});
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
 
-	todos.push(body);
-	res.json(body);
+	// todos.push(body);
+	// res.json(body);
 });
 
 app.delete('/todos/:id', function(req, res) {
@@ -106,7 +114,9 @@ app.put('/todos/:id', function(req, res) {
 	res.json(match);
 });
 
-app.listen(PORT, function() {
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function() {
 	console.log('Listening on ' + PORT);
 
 });
+})
