37f67d27a75f541a0a20882af13872c0cdd07b0b
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2016-03-02 01:42:22 -0800

    converted get todos/id to sequelize

--- server.js
+++ server.js
@@ -43,32 +43,25 @@ app.get('/todos', function(req, res) {
 })
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var match = _.findWhere(todos, {
-		id: todoId
-	})
-
-	match ? res.json(match) : res.send("404 error");
-
-
+	db.todo.findById(todoId).then(function(todo){
+		if(todo) {
+			res.json(todo.toJSON())
+		} else {
+			res.status(404).send();
+		}
+	}), function(e) {
+			res.status(500).send();
+		}
 });
 
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	db.todo.create(body).then(function(todo){
-		res.status(200)
-		res.json(todo.toJSON())
+		return res.status(200)
+		
 	}).catch(function(e){
-		res.status(400).json(e);
+		res.json(todo.toJSON())
 	});
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-	// res.json(body);
 });
 
 app.delete('/todos/:id', function(req, res) {
